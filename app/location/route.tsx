ed90d661d7c19160ff13b2dd2b6f29d750d0cb1e
noreplay <jekji125@protonmail.com>
2026-03-21 12:45:17 +0800

    IN

--- app/location/route.tsx
+++ app/location/route.tsx
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-	return NextResponse.json({ country: 'SG' });
-	// return NextResponse.json({ state: 'MH', country: 'IN' });
+	// return NextResponse.json({ country: 'SG' });
+	return NextResponse.json({ state: 'MH', country: 'IN' });
 	// return NextResponse.json({ state: 'MH', country: 'IN', city: 'Mumbai' });
 }
