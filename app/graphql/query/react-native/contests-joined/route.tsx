fc2978d0474088f15b38c49cad211290343aab8b
noreplay <jekji125@protonmail.com>
2026-03-21 17:25:50 +0800

    contentsJoined

---
+++ app/graphql/query/react-native/contests-joined/route.tsx
@@ -0,0 +1,71 @@
+import { BASEHEADERS } from '@/constants';
+import { extractAndModifyHeaders } from '@/lib/changeHeader';
+import { NextResponse } from 'next/server';
+
+/**
+:method: POST
+:path: /graphql/query/react-native/contests-joined
+:authority: www.dream11.com
+:scheme: https
+accept: application/json
+device: androidplaystore
+devicetype: ANDROID
+deviceid: 2d016ea7f014d06c
+ek1: 1baaed3d74f4a9d3e015539e9e57e52e3628b5eacd96f7e4fb42be4ef92153d9
+ek2: 1baaed3d74f4a9d3e015539e9e57e52e3628b5eacd96f7e4fb42be4ef92153d9
+x-manufacturer: google
+x-os-version: 16
+x-os-type: android
+x-app-version-name: 750
+app_version: 7.5.0
+a1: b4581fbf01c51d18e8c85cc70ed1b60f7e4467ade5695bfbc25b50e5b0cc84f5
+version: 10045
+user-agent: Dream11/7.5.0 (Linux; Android 36; Pixel 6a) Build/10045
+siteid: 1
+locale: en-US
+authorization: Bearer eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCIsInR5cCI6ImF0K2p3dCIsImtpZCI6IlRqb0FsLVdyZWN3Z3MtZVVvcm5xWWE5Y2x4dyJ9.eyJhdWQiOiJhOEVzRGxQYzNaQ1Znc1VwcHB1YyIsImV4cCI6MTc3MzY3NzY5NiwiaWF0IjoxNzczNTkxMjk2LCJpc3MiOiJkcmVhbTExLmNvbSIsInN1YiI6IjMxNzE1MjMzMCIsImp0aSI6ImhtT2tmc0NCQVM5d2JkQUhiSDVva3lCOW84OVpqdVRRIiwidGlkIjoiMiIsInJmdF9pZCI6IjBGQkFEMUQ5RjFENzdGOTlFQjdFNzAzM0IzQ0RFREVGIiwiY2xpZW50X2lkIjoiYThFc0RsUGMzWkNWZ3NVcHBwdWMiLCJzY29wZSI6ImRyZWFtMTE6YWxsIiwiYW1yIjpbIm90cCJdLCJjb3VudHJ5Q29kZSI6IklOIn0.iZVontVwx0-WkQErCRaKQ9Eum31miMaGnsO1uO4KOiuKxnGxl2nx_guM8k4pL0R8R7WRhTebkXXlC-WUb039AGU0kq6U-Hafrx40Pss3bsJ76AKG8Ygi8SN38-D3g-oXanhGdnnH0NPuoVDCUfBL5M9UmkIzvH_VwPOoJFCBRtsNoNzmmIN5Ef-YhdNi8sb2fvuXnIkzekJ0KbhxtpCTA6xcrFn3uemmJPyhyzg6lDU4pvDYAGpDiJh0GTnk1RXtDj9qqkloX4jSrJ5HQkqIBUlTtEKykImGSJz9ob-oXbpkajc0kcQKrCltfclX0eOHPHy98lQTgpvOtg6ulY6nvQ
+content-type: application/json
+content-length: 435
+accept-encoding: gzip
+
+{"query":"\n    query ContestHomePostRLBaseQuery($site: String!, $matchId: Int!, $shouldFetchSquads: Boolean = false) {\n  match(site: $site, id: $matchId) {\n    name\n    userTeamsCount\n    joinedContestsCount\n    joinedContestsMoreThanPageSize\n    status\n    startTime\n    squads @include(if: $shouldFetchSquads) {\n      shortName\n    }\n  }\n}\n    ","variables":{"matchId":112970,"site":"cricket","shouldFetchSquads":true}}
+
+*/
+export async function POST(request: Request) {
+	const body = await request.json();
+	const { query, variables } = body;
+	
+	const requestHeaders = extractAndModifyHeaders(request, process.env.WWW_GRAPHAL_URL || '');
+
+	if (process.env.WWW_GRAPHAL_URL) {
+		// Use real API to fetch data
+		try {
+			const apiURL = process.env.WWW_GRAPHAL_URL + "/graphql/query/react-native/contests-joined";
+			const response = await fetch(apiURL, {
+				method: 'POST',
+				headers: requestHeaders,
+				body: JSON.stringify({
+					query,
+					variables
+				}),
+				// Next.js 特有缓存配置：每 60 秒刷新一次数据
+				next: { revalidate: 60 }
+			});
+			
+			if (!response.ok) {
+				throw new Error(`API request failed: ${response.status}, url: ${apiURL}, headers: ${JSON.stringify(requestHeaders)}`);
+			}
+			
+			const data = await response.json();
+
+			console.log("contestsJoined", JSON.stringify(data));
+
+			return NextResponse.json(data);
+		} catch (error) {
+			console.error('API request error:', error);
+			// Fall back to mock data if API fails
+		}
+	}
+
+	return NextResponse.json({});
+}
